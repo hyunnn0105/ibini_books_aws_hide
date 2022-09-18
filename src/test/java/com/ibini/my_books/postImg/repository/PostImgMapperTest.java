23ce55b2a6a618ae2015637d95e875ea8796f16d
hyunnn0105 <102934972+hyunnn0105@users.noreply.github.com>
2022-09-18 21:42:47 +0900

    Update PostImgMapperTest.java

--- src/test/java/com/ibini/my_books/postImg/repository/PostImgMapperTest.java
+++ src/test/java/com/ibini/my_books/postImg/repository/PostImgMapperTest.java
@@ -13,95 +13,95 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class PostImgMapperTest {
 
-    @Autowired
-    PostImgMapper mapper;
+//     @Autowired
+//     PostImgMapper mapper;
 
-    @Test
-    @DisplayName("파일추가")
-    void addFile() {
-        PostImg postImg = new PostImg();
-        postImg.setFileName("333kjkj_kjkjk_상어.jpg");
-        postImg.setAccount("ibini");
-        postImg.setPostNo(2L);
-        postImg.setOriginalFileName("상어2.jpg");
+//     @Test
+//     @DisplayName("파일추가")
+//     void addFile() {
+//         PostImg postImg = new PostImg();
+//         postImg.setFileName("333kjkj_kjkjk_상어.jpg");
+//         postImg.setAccount("ibini");
+//         postImg.setPostNo(2L);
+//         postImg.setOriginalFileName("상어2.jpg");
 
-        mapper.addFile(postImg);
-    }
+//         mapper.addFile(postImg);
+//     }
 
-    @Test
-    @DisplayName("파일경로들을 조회한다.")
-    void findFileNamesTest() {
-        //given
-        Long postNo = 7L;
+//     @Test
+//     @DisplayName("파일경로들을 조회한다.")
+//     void findFileNamesTest() {
+//         //given
+//         Long postNo = 7L;
 
-        //when
-        List<String> fileNames = mapper.findFileNames(postNo);
+//         //when
+//         List<String> fileNames = mapper.findFileNames(postNo);
 
-        //then
-        for (String fileName : fileNames) {
-            System.out.println(fileName);
-        }
-//        assertEquals(2, fileNames.size());
-    }
+//         //then
+//         for (String fileName : fileNames) {
+//             System.out.println(fileName);
+//         }
+// //        assertEquals(2, fileNames.size());
+//     }
 
-    @Test
-    @DisplayName("포스트 번호로 postImg 객체 리스트를 반환한다")
-    void findFilesTest(){
-        Long postNo = 1L;
+//     @Test
+//     @DisplayName("포스트 번호로 postImg 객체 리스트를 반환한다")
+//     void findFilesTest(){
+//         Long postNo = 1L;
 
-        List<PostImg> fileList = mapper.findFiles(postNo);
-        for (PostImg postImg : fileList) {
-            System.out.println(postImg);
-        }
-    }
-    @Test
-    @DisplayName("썸네일이 true인 postImg 객체 리스트를 조회한다")
-    void findThumbsTest(){
-        List<PostImg> thumbs = mapper.findThumbs();
-        for (PostImg thumb : thumbs) {
-            System.out.println(thumb);
-        }
-    }
-    //
-//    boolean (String fileName);
-    @Test
-    @DisplayName("fileName 으로 postImg DB에서 삭제")
-    void removeByNameTest(){
-        String fileName = "2222kjkj_kjkjk_상어.jpg";
-        boolean flag = mapper.removeByName(fileName);
-        assertTrue(flag);
-    }
+//         List<PostImg> fileList = mapper.findFiles(postNo);
+//         for (PostImg postImg : fileList) {
+//             System.out.println(postImg);
+//         }
+//     }
+//     @Test
+//     @DisplayName("썸네일이 true인 postImg 객체 리스트를 조회한다")
+//     void findThumbsTest(){
+//         List<PostImg> thumbs = mapper.findThumbs();
+//         for (PostImg thumb : thumbs) {
+//             System.out.println(thumb);
+//         }
+//     }
+//     //
+// //    boolean (String fileName);
+//     @Test
+//     @DisplayName("fileName 으로 postImg DB에서 삭제")
+//     void removeByNameTest(){
+//         String fileName = "2222kjkj_kjkjk_상어.jpg";
+//         boolean flag = mapper.removeByName(fileName);
+//         assertTrue(flag);
+//     }
 
-    @Test
-    @DisplayName("post_no으로 postImg DB에서 삭제")
-    void removeByPostNoTest(){
-        Long postNo = 1L;
-        boolean flag = mapper.removeByPostNo(postNo);
-        assertTrue(flag);
-    }
-    @Test
-    @DisplayName("postNo 값이 있고 thumbnail이 true인 PostImg를 DB에 저장")
-    void addFileTest2(){
-        PostImg pi = new PostImg();
-        pi.setFileName("testtesttest");
-        pi.setPostNo(32L);
-        pi.setAccount("2208260002");
-        pi.setOriginalFileName("test");
-        pi.setThumbnail("true");
+//     @Test
+//     @DisplayName("post_no으로 postImg DB에서 삭제")
+//     void removeByPostNoTest(){
+//         Long postNo = 1L;
+//         boolean flag = mapper.removeByPostNo(postNo);
+//         assertTrue(flag);
+//     }
+//     @Test
+//     @DisplayName("postNo 값이 있고 thumbnail이 true인 PostImg를 DB에 저장")
+//     void addFileTest2(){
+//         PostImg pi = new PostImg();
+//         pi.setFileName("testtesttest");
+//         pi.setPostNo(32L);
+//         pi.setAccount("2208260002");
+//         pi.setOriginalFileName("test");
+//         pi.setThumbnail("true");
 
-        mapper.addFile(pi);
-    }
-    @Test
-    @DisplayName("postNo 값이 있고 thumbnail이 null인 PostImg를 DB에 저장")
-    void addFileTest3(){
-        PostImg pi = new PostImg();
-        pi.setFileName("testtesttesttest");
-        pi.setPostNo(32L);
-        pi.setAccount("2208260002");
-        pi.setOriginalFileName("testtest");
+//         mapper.addFile(pi);
+//     }
+//     @Test
+//     @DisplayName("postNo 값이 있고 thumbnail이 null인 PostImg를 DB에 저장")
+//     void addFileTest3(){
+//         PostImg pi = new PostImg();
+//         pi.setFileName("testtesttesttest");
+//         pi.setPostNo(32L);
+//         pi.setAccount("2208260002");
+//         pi.setOriginalFileName("testtest");
 
-        mapper.addFile(pi);
-    }
+//         mapper.addFile(pi);
+//     }
 
 //    @Test
 //    @DisplayName("썸네일이 있으면 수정하고 없으면 추가한다")
